449bef6279d77e6f2a0f8cd399cd3962c6ec150d
Jui-Yasmin <jui.yasmin83@gmail.com>
2023-08-29 14:28:30 +0600

    This is a variable file

---
+++ variables/Add Variables Together.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5, y = 6, ;
+  printf("%d", z=x+y);
+  return 0;
+}
---
+++ variables/j.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  int x = 9, y = 8 ;
+  printf("%d", x+y);
+  return 0;
+}
