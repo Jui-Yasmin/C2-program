cf0a737187452850b28b190cc847c64880fa1abd
Jui-Yasmin <jui.yasmin83@gmail.com>
2023-10-26 10:19:39 +0600

    Boolean file

--- booleans/booleans3.c
+++ booleans/booleans3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int x = 10;
-  int y = 9;
+  int x = 110;
+  int y = 90;
 
   printf("%d", x > y); // Returns 1 (true) because 10 is greater than 9
   return 0;
