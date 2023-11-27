27788dc4fcfec2bec5d3779c426008a01a7f4f92
Jui-Yasmin <jui.yasmin83@gmail.com>
2023-11-27 18:35:50 +0600

    Definition file

--- multidimensional-arrays/multidimensional-arrays2.c
+++ multidimensional-arrays/multidimensional-arrays2.c
@@ -2,7 +2,8 @@
 
 int main() {
   int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
-  printf("%d", matrix[1][2]);
+  matrix[0][0] = 9;
+  printf("%d", matrix[0][0]);  // Now outputs 9 instead of 1
 
   return 0;
 }
