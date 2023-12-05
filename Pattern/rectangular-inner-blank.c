79a7e6b4fd6884e36450848bd2c28be367c445f3
Jui-Yasmin <jui.yasmin83@gmail.com>
2023-12-05 06:54:05 +0600

    Pattern file

---
+++ Pattern/rectangular-inner-blank.c
@@ -0,0 +1,26 @@
+int main()
+{
+    int rows, columns;
+
+    printf("Enter the number of rows & columns:\n");
+    scanf("%d%d", &rows, &columns);
+
+    printf("\n");
+    if (rows > 0 && columns > 0)
+    {
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= columns; j++)
+            {
+                if (i == 1 || i == rows || j == 1 || j == columns)
+                    printf("* ");
+                else
+                    printf("  ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
+
