d3e721b2e09facd730540b46473ad1e543111ac0
Jui-Yasmin <jui.yasmin83@gmail.com>
2023-12-06 06:30:43 +0600

    Pattern file

---
+++ Pattern/rectangle-2diagonal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            // inside inner loop print star for first and last rows or for first and last column
+            //  using j==n-i+1 || i==j for the diagonal
+
+            if (i == 1 || i == n || j == 1 || j == n || j==n-i+1 || i==j)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+return 0;
+}
