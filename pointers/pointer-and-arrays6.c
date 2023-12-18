ee1d1af3989626d22b1df7bcc80ec5f8a9e744dd
Jui-Yasmin <jui.yasmin83@gmail.com>
2023-12-18 18:27:35 +0600

    Memory-address(pointers&array) file

---
+++ pointers/pointer-and-arrays6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNumbers[5] = {25, 50, 75, 100};
+
+    // Get the value of the first element in myNumbers
+    printf("%d", *myNumbers);
+
+    return 0;
+}
