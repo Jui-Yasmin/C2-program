2c2948de0fd33303f97ba979aa7d5ac61f91d8e6
Jui-Yasmin <jui.yasmin83@gmail.com>
2024-01-03 06:46:24 +0600

    This is a function file

--- function/call-a-function2.c
+++ function/call-a-function2.c
@@ -3,11 +3,13 @@
 // Create a function
 void myFunction()
 {
-    printf("I just got executed!....");
+    printf("I just got executed!\n");
 }
 
 int main()
 {
+    myFunction(); // call the function
+    myFunction(); // call the function
     myFunction(); // call the function
     return 0;
 }
